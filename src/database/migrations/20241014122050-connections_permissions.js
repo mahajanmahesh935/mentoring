16b5d54a565c874b679c4f0680c4dcc620373795
nevil <nevil@tunerlabs.com>
2024-10-14 18:14:33 +0530

    added: routes config and role permission mapping

---
+++ src/database/migrations/20241014122050-connections_permissions.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		try {
+			const permissionsData = [
+				{
+					code: 'connections_permissions',
+					module: 'connections',
+					request_type: ['POST', 'GET'],
+					api_path: '/mentoring/v1/connections/*',
+					status: 'ACTIVE',
+					created_at: new Date(),
+					updated_at: new Date(),
+				},
+			]
+			await queryInterface.bulkInsert('permissions', permissionsData)
+		} catch (error) {
+			console.log(error)
+		}
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.bulkDelete('permissions', null, {})
+	},
+}
